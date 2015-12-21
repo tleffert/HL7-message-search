114cb5b50496d47a95a1d100fc0cabe0b6b443a1
tleffert <leffertt@gmail.com>
2015-12-21 02:12:03 -0600

    Code cleanup and documentation

--- app/directives/directives.js
+++ app/directives/directives.js
@@ -1,3 +1,7 @@
+
+//Directive that removes the default HL7 syntax from the message
+
+//USAGE ex: <div ng-repeat="message in messages" formathl7="<message>"></div>
 myApp.directive('formathl7',
     function($filter,$sce){
         return {
@@ -5,14 +9,15 @@ myApp.directive('formathl7',
             scope: { formathl7: '='},
             link: function(scope) {
                 var message = scope.formathl7.split('\n');
-                var line;
                 var rebuiltMessage = '';
 
+                //Regexs to find '|' and '^' in a message;
                 var fieldSeparator = new RegExp('\\|+','g');
                 var componentCharJoin = new RegExp('\\^+','g');
 
-                for(line in message) {
-                        rebuiltMessage += message[line].replace(fieldSeparator, ' ').replace(componentCharJoin,',') + '\n';
+                //Replace/removing default HL7 syntax
+                for(var segmentIndex in message) {
+                        rebuiltMessage += message[segmentIndex].replace(fieldSeparator, ' ').replace(componentCharJoin,',') + '\n';
                     }
                scope.formathl7 = rebuiltMessage;
             }
--- app/filters/filters.js
+++ app/filters/filters.js
@@ -1,3 +1,6 @@
+
+//Angular filter that will highlight a list of search terms
+//USAGE ex: <div ng-repeat="message in messages | highlight:<searchtermlist>"></div>
 myApp.filter('highlight',
     function($sce) {
         return function(str, terms) {
@@ -5,36 +8,38 @@ myApp.filter('highlight',
             if(terms && str){
                 // want to separate the terms so that we can search for each individually
                 var termList = terms.split(',');
-                var lines = str.split('\n');
-                var line;
+                var message = str.split('\n');
                 var textToReturn='';
                    var tempList = [];
-                   for(var termIndex in termList) {
-                        var emptyRegex = new RegExp('^\\s$');
 
+                   //checking for empty strings, they need to be removed from list of terms to search for
+                   for(var termIndex in termList) {
                         if(termList[termIndex]) {
                             tempList.push(termList[termIndex]);
                         }
                    }
-                   termList = tempList;
-                    for(line in lines) {
-                        //search for term, but do it globally-ignorecase ('is' matches 'is' and 'this' and 'THIS')
+                  termList = tempList;
+                    //Where the actual highlighting occurs, just wraps the match in a span
+                    for(var segmentIndex in message) {
+                        //search for terms using 'or' logic ('|'), but do it globally-ignorecase ('is' matches 'is' and 'this' and 'THIS')
                         var regex = new RegExp('(' + termList.join('|') + ')', 'gi');
                         //applying the highlight to the case match
-                        textToReturn += '<span>'
-                        textToReturn +=lines[line].replace(regex, '<span class="highlight">$&</span>');
-                        textToReturn += '</span></br>'
+                        textToReturn += '<span class="segment-block"><p>'
+                        textToReturn +=message[segmentIndex].replace(regex, '<span class="highlight">$&</span>');
+                        textToReturn += '</p></span>'
 
                     }
                 //need to specify that we has trusted html being sent back to the html bind.
                 return $sce.trustAsHtml(textToReturn);
             } else if(str) {
-                var lines = str.split('\n');
+                //If there are no terms (case is initial load more often than not) still want to dress the message up a bit for readability
 
-               for(var line in lines) {
+                var message = str.split('\n');
+
+               for(var segmentIndex in message) {
                    //search for term, but do it globally-ignorecase ('is' matches 'is' and 'this' and 'THIS')
                    //applying the highlight to the case match
-                   textToReturn += '<span>' + lines[line] + '</span></br>';
+                   textToReturn += '<span class="segment-block"><p>' + message[segmentIndex] + '</p></span>'
 
                }
                return $sce.trustAsHtml(textToReturn);
@@ -44,6 +49,8 @@ myApp.filter('highlight',
         };
 });
 
+//Angular filter that filters a list of results, based off of allowing multiple search terms
+//USAGE ex: <div ng-repeat="message in messages | filterMultiple:<searchtermlist>"></div>
 myApp.filter('filterMultiple',
     function($sce) {
         return function(messages,terms) {
@@ -56,23 +63,27 @@ myApp.filter('filterMultiple',
                var termList = terms.split(',');
                var tempList = termList;
                 var tempList = [];
-                  for(var termIndex in termList) {
-                       var emptyRegex = new RegExp('^\\s$');
 
+                //checking for empty strings, they need to be removed from list of terms to search for
+                  for(var termIndex in termList) {
                        if(termList[termIndex]) {
                            tempList.push(termList[termIndex]);
                        }
                   }
                   termList = tempList;
+
                for(var messageIndex in messages) {
                 var message = messages[messageIndex].message.split('\n');
                     match = false;
+
+                    //Looking for the search terms in each message using 'or' logic
                     for(var line in message) {
-                        //search for term, but do it globally-ignorecase ('is' matches 'is' and 'this' and 'THIS')
+                        //search for terms using 'or' logic ('|'), but do it globally-ignorecase ('is' matches 'is' and 'this' and 'THIS')
                         var regex = new RegExp('(' + termList.join('|') + ')', 'gi');
-                        //applying the highlight to the case match
+                        //applying 'or' logic to determin to show message
                         match = match || regex.test(message[line]);
                     }
+                    //if a message contains atleast one of the terms add to results list to hand back to page
                     if(match) {
                         matches.push(messages[messageIndex]);
                     }
